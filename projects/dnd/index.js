ebc316c614a8046aa16e388066677717a819f123
Igor Rodin <igreen1974@gmail.com>
2021-08-10 21:24:39 +0300

    ДЗ 6 - Drag and drop задача

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -21,11 +21,81 @@ const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+homeworkContainer.style.height = '100vh';
+const hwContRect = homeworkContainer.getBoundingClientRect();
+
+export function createDiv() {
+  const getRandomNumber = (from, to) => from + Math.round(Math.random() * to);
+
+  const box = document.createElement('div');
+  box.classList.add('draggable-div');
+
+  const boxWidth = getRandomNumber(10, 0.6 * hwContRect.width);
+  const boxHeight = getRandomNumber(10, 0.6 * hwContRect.height);
+
+  box.style.width = `${boxWidth}px`;
+  box.style.height = `${boxHeight}px`;
+  box.style.left = `${getRandomNumber(0, hwContRect.width - boxWidth)}px`;
+  box.style.top = `${getRandomNumber(0, hwContRect.height - boxHeight)}px`;
+
+  box.style.backgroundColor = `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(
+    0,
+    255
+  )}, ${getRandomNumber(0, 255)})`;
+  box.draggable = 'true';
+
+  return box;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
+
+let selectedBox, shiftX, shiftY;
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
+});
+
+homeworkContainer.addEventListener('dragstart', (event) => {
+  selectedBox = event.target;
+  shiftX = event.clientX - parseInt(selectedBox.style.left);
+  shiftY = event.clientY - parseInt(selectedBox.style.top);
+});
+
+homeworkContainer.addEventListener('dragover', (event) => {
+  event.preventDefault();
+});
+
+homeworkContainer.addEventListener('drop', (event) => {
+  if (selectedBox) {
+    event.preventDefault();
+
+    if (event.clientX - shiftX <= 0) {
+      selectedBox.style.left = `0px`;
+    } else if (
+      event.clientX + parseInt(selectedBox.style.width) - shiftX >=
+      hwContRect.width
+    ) {
+      selectedBox.style.left = `${
+        hwContRect.width - parseInt(selectedBox.style.width)
+      }px`;
+    } else {
+      selectedBox.style.left = `${event.clientX - shiftX}px`;
+    }
+
+    if (event.clientY - shiftY <= 0) {
+      selectedBox.style.top = `0px`;
+    } else if (
+      event.clientY + parseInt(selectedBox.style.height) - shiftY >=
+      hwContRect.height
+    ) {
+      selectedBox.style.top = `${
+        hwContRect.height - parseInt(selectedBox.style.height)
+      }px`;
+    } else {
+      selectedBox.style.top = `${event.clientY - shiftY}px`;
+    }
+
+    selectedBox = null;
+  }
 });
